38989292be1aa489cf3e0c4d3fd172e5b51792a6
Brandon Fosdick <bfoz@bfoz.net>
2010-04-21 16:06:11 +0200

    First attempt at the API
    Doesn't yet support submatches or match locations
    
    For find() functions, the expression(s) must match anywhere in the string (or input stream).
     
    For match() functions, the expression(s) must match the entire string (or immediately match the input stream).

---
+++ regex/regex.h
@@ -0,0 +1,37 @@
+#ifndef	REGEX_H
+#define	REGEX_H
+
+#include <list>
+#include <string>
+
+#include "expression.h"
+
+namespace regex
+{
+    // Find the left-most match in the string
+    bool find(const std::string& s, expression& re);
+
+    // Read from the input stream until EOS or a match is found
+    bool find(std::istream* is, expression& re);
+
+    // Try to match the expression against the entire string
+    bool match(const std::string& s, const expression& re);
+
+    // Try to match the expression against the input stream
+    bool match(std::istream& is, expression& re);
+
+
+    // Find the left-most matching expressions in the string
+    expressions* find(const std::string& s, expressions& regexs);
+
+    // Read from the input stream until EOS or a match is found
+    expressions* find(std::istream& is, expressions& regexs);
+
+    // Return the expressions that match the entire input string
+    expressions* match(const std::string& s, expressions& regexs);
+
+    // Return the expressions that match the input stream
+    expressions* match(std::istream& is, expressions& regexs);
+}
+
+#endif	// REGEX_H

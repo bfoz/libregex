6327708a088bcb89647594573b07b04e7ed65c29
Brandon Fosdick <bfoz@bfoz.net>
2010-05-03 16:33:30 +0200

    Add merge() functions
    Implement merging of state machine fragments to support alternations

---
+++ regex/state.cc
@@ -0,0 +1,62 @@
+#include "state.h"
+
+namespace regex
+{
+    state::base* state::merge(const_states& s, const base& left, const base& right)
+    {
+	// Check for identity
+	if( &left == &right )
+	    return const_cast<base*>(&left);
+
+	// Check for a state::final
+	if( left.isFinal() )
+	    return merge(s, (literal&)right, (final&)left);
+	else if( right.isFinal() )
+	    return merge(s, (literal&)left, (final&)right);
+
+	return merge(s, (literal&)left, (literal&)right);
+    }
+
+    state::literal* state::merge(const_states& s, const literal& s1, const literal& s2)
+    {
+	literal* m = new literal;
+
+	// Add the new state to the state list
+	s.push_back(m);
+
+	// Merge the two transition maps, handling any duplicates
+	literal::const_iterator i = s1.begin();
+	for(; i != s1.end(); ++i)
+	{
+	    if( s2.next(i->first) )	// Does s2 have a duplicate transition?
+	    {
+		// Make a new state to handle the alternation
+		m->insert(i->first, merge(s, *(i->second), *(s2.next(i->first))));
+	    }
+	    else
+		m->insert(i->first, i->second);
+	}
+
+	// Merge all remaining transitions in s2
+	for(i = s2.begin(); i != s2.end(); ++i)
+	    if( !m->next(i->first) )
+		m->insert(i->first, i->second);
+
+	return m;
+    }
+
+    state::literal* state::merge(const_states& s, const literal& s1, const final& s2)
+    {
+	literal* m = new literal(&s2);
+
+	// Add the new state to the state list
+	s.push_back(m);
+
+	// Copy all transitions from the literal state
+	literal::const_iterator i = s1.begin();
+	for(; i != s1.end(); ++i)
+	    m->insert(i->first, i->second);
+
+	return m;
+    }
+}
--- regex/state.h
+++ regex/state.h
@@ -15,28 +15,42 @@ namespace regex
 
     typedef std::list<state::base*>		states;		// A container for state objects
     typedef std::list<const state::base*>	const_states;	// A container for const state objects
+
+    namespace state
+    {
+	base* merge(const_states&, const base&, const base&);		// Merge two states
+	literal* merge(const_states&, const literal&, const literal&);	// Merge two literal states
+	literal* merge(const_states&, const literal&, const final&);	// Merge a literal state with a final state
+    }
 }
 
 class regex::state::base
 {
 public:
     virtual const base*	getDefault() const	{ return NULL;	}
+    virtual bool	isFinal() const		{ return false;	}
     virtual const base* next(char) const =0;
 };
 
 class regex::state::literal : public base
 {
-    typedef std::map<char, const base*>	transition_type;
+    typedef std::map<char, const base*>	transitions_type;
 
     const base*	_default;
-    transition_type transitions;
+    transitions_type transitions;
 
 public:
+    typedef transitions_type::const_iterator	const_iterator;
+    typedef transitions_type::iterator	iterator;
+
     literal(const base* def=NULL) : _default(def) {}
     literal(char c, const base* target) : _default(NULL)
     {
 	insert(c, target);
     }
+
+    const_iterator begin() const { return transitions.begin();	 }
+    const_iterator end() const { return transitions.end();	 }
 
     void insert(char c, const base* target)
     {
@@ -45,7 +59,7 @@ public:
 
     virtual const base* next(char c) const
     {
-	transition_type::const_iterator i(transitions.find(c));
+	transitions_type::const_iterator i(transitions.find(c));
 	if( transitions.end() == i )
 	    return _default;
 	return i->second;
@@ -58,6 +72,7 @@ public:
 class regex::state::final : public base
 {
 public:
+    virtual bool isFinal() const	{ return true;	}
     virtual base* next(char) const { return NULL; }
 };
 
